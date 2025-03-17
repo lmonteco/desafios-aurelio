86fdc555cc19212a1035ba58bfae05d4e5dd0053
lucas.monteiro <lmontecosta@gmail.com>
2025-03-17 14:31:55 -0300

    Alterar exemplo de CNPJ

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ function removeCaracteresEspeciaisCNPJ(cnpj) {
     let cnpjSemCaracterEspecial = cnpj.replace(/[^0-9]/g, '');
     return cnpjSemCaracterEspecial;
 }
-console.log(removeCaracteresEspeciaisCNPJ('57.826.072/0001-00AAAAAAAAAAABBB'))
+console.log(removeCaracteresEspeciaisCNPJ('57.826.072/0001-BABSDOIIUAHSOIDH'))
