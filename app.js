559e929bd6fdb8735b4b5119f440d32ab0eb6005
lmonteco <76770414+lmonteco@users.noreply.github.com>
2025-03-17 14:11:22 -0300

    Corrigir função para receber apenas números no CNPJ

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function removeCaracteresEspeciaisCNPJ(cnpj) {
-    let cnpjSemCaracterEspecial = cnpj.replace(/[^a-zA-Z0-9]/g, '');
+    let cnpjSemCaracterEspecial = cnpj.replace(/[^0-9]/g, '');
     return cnpjSemCaracterEspecial;
 }
 console.log(removeCaracteresEspeciaisCNPJ('57.826.072/0001-00AAAAAAAAAAABBB'))
