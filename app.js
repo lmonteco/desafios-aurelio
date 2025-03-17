ab77b15b26a789aa07a978f29a60bb6fd56c43c0
AJ <aurelin95@gmail.com>
2025-03-17 14:14:32 -0300

    feat: [AJ]: add v2

--- app.js
+++ app.js
@@ -2,4 +2,21 @@ function removeCaracteresEspeciaisCNPJ(cnpj) {
     let cnpjSemCaracterEspecial = cnpj.replace(/[^a-zA-Z0-9]/g, '');
     return cnpjSemCaracterEspecial;
 }
-console.log(removeCaracteresEspeciaisCNPJ('57.826.072/0001-00AAAAAAAAAAABBB'))
+
+function removeCaracteresEspeciaisCNPJv2(cnpj) {
+    let cnpjFormatado = '';
+   
+    for(i = 0 ; i < cnpj.length ; i++) {
+      if ( !isNaN(cnpj[i]) ) {
+        cnpjFormatado += cnpj[i];
+      }
+    }
+   
+    return cnpjFormatado;
+  }
+
+console.log({
+    v1: removeCaracteresEspeciaisCNPJ('57.826.072/0001-00AAAAAAAAAAABBB'),
+    v2: removeCaracteresEspeciaisCNPJv2('57.826.072/0001-00AAAAAAAAAAABBB')
+});
+
