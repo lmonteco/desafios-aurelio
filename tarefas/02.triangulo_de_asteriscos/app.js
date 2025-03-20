9d5db5ccd6026966fb6f7572da6951324a2c273a
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-20 15:24:39 -0300

    fix: Ajusta a função de gerar triangulos

--- tarefas/02.triangulo_de_asteriscos/app.js
+++ tarefas/02.triangulo_de_asteriscos/app.js
@@ -1,13 +1,14 @@
 let ladosTriangulo = prompt('Digite o número de asteriscos que deseja no triângulo: ');
-if (ladosTriangulo > 10 || ladosTriangulo < 3) {
+let numero = parseInt(ladosTriangulo);
+if (numero > 10 || numero < 3) {
   alert('Amigo, ajuda aí né! Escolha um número entre 3 e 10');
 } else {
-  function gerarTriangulo(ladosTriangulo) {
+  function gerarTriangulo(numero) {
     let asteriscoTriangulo = '';
-    for (i = 1; i <= ladosTriangulo; i++) {
+    for (i = 1; i <= numero; i++) {
       let triangulo = asteriscoTriangulo += '*';
-      console.log(triangulo);    
+      console.log(triangulo);
     }
-  gerarTriangulo(ladosTriangulo);
-}
+  }    
+      gerarTriangulo(numero);
 }
