bf9bd6d2bd013361ebf2b6fd1421ef0432e030be
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-20 15:06:26 -0300

    feat: Gera triangulo com asteriscos atraves de parametro

--- tarefas/02.triangulo_de_asteriscos/app.js
+++ tarefas/02.triangulo_de_asteriscos/app.js
@@ -1,3 +1,13 @@
-function gerarTriangulo(numero) {
-  // TODO
+let ladosTriangulo = prompt('Digite o número de asteriscos que deseja no triângulo: ');
+if (ladosTriangulo > 10 || ladosTriangulo < 3) {
+  alert('Amigo, ajuda aí né! Escolha um número entre 3 e 10');
+} else {
+  function gerarTriangulo(ladosTriangulo) {
+    let asteriscoTriangulo = '';
+    for (i = 1; i <= ladosTriangulo; i++) {
+      let triangulo = asteriscoTriangulo += '*';
+      console.log(triangulo);    
+    }
+  gerarTriangulo(ladosTriangulo);
+}
 }
