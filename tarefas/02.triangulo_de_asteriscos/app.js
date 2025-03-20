a0a220a31f393bbb4be3a0aadee8f6d72981144e
Lucas Monteiro <lmontecosta@gmail.com>
2025-03-20 15:27:46 -0300

    docs: Melhora a identação do código

--- tarefas/02.triangulo_de_asteriscos/app.js
+++ tarefas/02.triangulo_de_asteriscos/app.js
@@ -1,14 +1,16 @@
 let ladosTriangulo = prompt('Digite o número de asteriscos que deseja no triângulo: ');
 let numero = parseInt(ladosTriangulo);
+
 if (numero > 10 || numero < 3) {
   alert('Amigo, ajuda aí né! Escolha um número entre 3 e 10');
 } else {
-  function gerarTriangulo(numero) {
-    let asteriscoTriangulo = '';
-    for (i = 1; i <= numero; i++) {
-      let triangulo = asteriscoTriangulo += '*';
-      console.log(triangulo);
-    }
-  }    
-      gerarTriangulo(numero);
+  gerarTriangulo(numero);
+}
+
+function gerarTriangulo(numero) {
+  let asteriscoTriangulo = '';
+  for (i = 1; i <= numero; i++) {
+    let triangulo = asteriscoTriangulo += '*';
+    console.log(triangulo);
+  }
 }
